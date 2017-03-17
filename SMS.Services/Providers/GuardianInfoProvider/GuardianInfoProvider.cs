c74f808f04d8efae8571e61d2158c2de7e45952a
Manoj <mj5500093gmail.com>
2017-03-17 12:44:10 +0545

    first

--- SMS.Services/Providers/GuardianInfoProvider/GuardianInfoProvider.cs
+++ SMS.Services/Providers/GuardianInfoProvider/GuardianInfoProvider.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using SMS.Models.GuardianInfoViewModel;
+using SMS.Entities;
 namespace SMS.Services.Providers
 {
-    class GuardianInfoProvider
+    public interface IGuardianInfoProvider
     {
+        bool Save(GuardianInfoViewModel model);
+        GuardianInfoViewModel Edit(int guardianId);
+        bool Delete(int guardianId);
+
+    }
+    public class GuardianInfoProvider : IGuardianInfoProvider
+    {
+        public bool Delete(int guardianId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public GuardianInfoViewModel Edit(int guardianId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save(GuardianInfoViewModel model)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
